e5c7286e8b32661ee15e4abff96ce3299f66f5c3
S_ran <sarankumar2603@gmail.com>
2026-07-29 08:57:51 -0700

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,8 +20,8 @@ function About() {
 
         <div className="about-text">
           <p className="text-[0.95rem] leading-relaxed text-text-muted">
-            I am **Sarankumar M**, a BCA graduate with a strong interest in building clean, practical, and user-friendly software solutions. I completed a **Junior Software Developer** course at **CADD Technology**, where I strengthened my foundation in software development and modern programming concepts.
-I also have professional experience as a **PHP Developer** at my previous company, where I contributed to both front-end and back-end development for internal company applications. While my career began with PHP, I also have a strong interest in **Java** and **React** and am actively enhancing my skills by building projects and exploring modern development practices.
+            I am <strong>Sarankumar M</strong>, a BCA graduate with a strong interest in building clean, practical, and user-friendly software solutions. I completed a <strong>Junior Software Developer</strong> course at <strong>CADD Technology</strong>, where I strengthened my foundation in software development and modern programming concepts.
+I also have professional experience as a <strong>PHP Developer</strong> at my previous company, where I contributed to both front-end and back-end development for internal company applications. While my career began with PHP, I also have a strong interest in <strong>Java</strong> and <strong>React</strong> and am actively enhancing my skills by building projects and exploring modern development practices.
 I enjoy creating efficient, scalable, and user-friendly web applications, and I am always eager to learn new technologies, solve real-world challenges, and grow as a software developer.
           </p>
         </div>
